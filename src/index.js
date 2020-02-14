7cd785bce4e7f5aeb9c8acd502250f164233b03f
Mohammad Ahmady Yeganeh <m.yegane49@outlook.com>
2020-02-14 22:24:24 +0330

    Continue on until finishing the dashboard

--- src/index.js
+++ src/index.js
@@ -1,5 +1,10 @@
 import './scss/style.scss';
 import 'bootstrap';
 import jQuery from 'jquery';
+import ClassicEditor from '@ckeditor/ckeditor5-build-classic';
+
+ClassicEditor.create( document.querySelector( '#editor1' ));
+    
+    
 
 jQuery('#year').text(new Date().getFullYear());
